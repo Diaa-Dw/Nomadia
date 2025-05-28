36e4397984e627f00c25408271aa2e0975bca7c8
Diaa-Dw <oava9944@gmail.com>
2025-05-29 01:11:11 +0300

    add ThemeContext and globalStyles for theme management

---
+++ src/theme/ThemeContext.ts
@@ -0,0 +1,12 @@
+import { createContext, useContext } from 'react';
+import type { ThemeContextType } from './';
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useThemeContext = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useThemeContext must be used within a ThemeProvider');
+  }
+  return context;
+};
