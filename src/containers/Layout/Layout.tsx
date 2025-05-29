0d131a33e5957e64853f2c36ef4f5127ea44017f
Diaa-Dw <oava9944@gmail.com>
2025-05-29 23:57:56 +0300

    Add Layout component with styled container for layout structure

---
+++ src/containers/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import { LayoutContainer } from './Layout.style';
+
+const Layout = () => {
+  return (
+    <LayoutContainer>
+      <Outlet />
+    </LayoutContainer>
+  );
+};
+
+export default Layout;
