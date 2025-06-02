451fd5831068cdd3757b181a0e3c24141fca5d43
Diaa-Dw <oava9944@gmail.com>
2025-06-02 09:36:58 +0100

    Update theme components and typography: enhance MuiButton styles with theme integration and adjust body2 font size

--- src/theme/components.ts
+++ src/theme/components.ts
@@ -1,6 +1,6 @@
-import type { Components } from '@mui/material';
+import type { Components, Theme } from '@mui/material';
 
-const components: Components = {
+const components: Components<Theme> = {
   MuiButton: {
     styleOverrides: {
       root: {
@@ -8,23 +8,50 @@ const components: Components = {
         padding: '8px 16px',
         textTransform: 'none',
       },
-      contained: {
+      contained: ({ theme }) => ({
         boxShadow: 'none',
+        color: theme.palette.background.default,
+        fontWeight: '600',
+        backgroundImage: `linear-gradient(to right, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
         '&:hover': {
           boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
         },
+        '&.Mui-disabled': {
+          backgroundImage: `linear-gradient(to right, ${theme.palette.grey[400]}, ${theme.palette.grey[500]})`,
+          color: theme.palette.action.disabled,
+          opacity: 0.7,
+        },
+      }),
+    },
+  },
+  MuiOutlinedInput: {
+    styleOverrides: {
+      root: {
+        borderRadius: '8px',
+        padding: '4px 8px',
       },
     },
   },
   MuiTextField: {
     styleOverrides: {
-      root: {
+      root: ({ theme }) => ({
+        padding: '4px 0',
+        borderRadius: '8px',
         color: '#fff',
-        padding: '8px 12px',
         '& input': {
-          padding: '8px',
+          padding: '4px',
+          borderRadius: '8px',
         },
-      },
+        '& input:-webkit-autofill': {
+          boxShadow: `0 0 0px 1000px ${theme.palette.background.paper} inset`,
+          WebkitBoxShadow: `0 0 0px 1000px ${theme.palette.background.paper} inset`,
+          border: '0 !important',
+          outline: '0 !important',
+          backgroundClip: 'content-box !important',
+          filter: 'none !important',
+          WebkitTextFillColor: theme.palette.text.primary,
+        },
+      }),
     },
   },
   MuiAppBar: {
