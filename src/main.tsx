fa9df944959595a7ef25ed59d91e98664eeb6946
Diaa-Dw <oava9944@gmail.com>
2025-06-03 16:48:31 +0300

    Add queryClient setup and integrate with React Query in main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from './theme';
 import { BrowserRouter } from 'react-router-dom';
+import { queryClient } from './lib';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
