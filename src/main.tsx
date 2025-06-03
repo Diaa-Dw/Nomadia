2a0475dcdd15df5e9c280566a6c581a0d2391d47
Diaa-Dw <oava9944@gmail.com>
2025-06-03 18:03:21 +0300

    Add Redux Toolkit and configure store; integrate with React in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import { ThemeProvider } from './theme';
 import { BrowserRouter } from 'react-router-dom';
 import { queryClient } from './lib';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux';
+import store from './store/index.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <Provider store={store}>
+            <App />
+          </Provider>
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </BrowserRouter>
