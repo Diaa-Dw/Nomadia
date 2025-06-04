11d46dace2c9097e625f08bf103f1a539bb006e6
Diaa-Dw <oava9944@gmail.com>
2025-06-05 00:34:34 +0300

    Add token utility functions for JWT parsing and expiration checks.

--- src/routes/PrivateWrapper.tsx
+++ src/routes/PrivateWrapper.tsx
@@ -1,8 +1,14 @@
-import { getAuthToken } from '../utils';
+import { Loader } from '@/containers';
+import { selectUser } from '@/features';
+import useVerifyToken from '@/hooks/userVerifyToken';
+import { useAppSelector } from '@/store';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateWrapper = () => {
-  const isAuthenticated = Boolean(getAuthToken());
+  const { isAuthenticating } = useVerifyToken();
+  const isAuthenticated = useAppSelector(selectUser);
+
+  if (isAuthenticating) return <Loader />;
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
