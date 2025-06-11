68b8218725a287349149763ea69ead4d196efbe6
Diaa-Dw <oava9944@gmail.com>
2025-06-11 15:22:22 +0300

    Add formatDisplayDate utility function for date formatting

---
+++ src/utils/date.ts
@@ -0,0 +1,7 @@
+export const formatDisplayDate = (date: Date): string => {
+  return date.toLocaleDateString('en-US', {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+  });
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './toast';
 export * from './auth';
 export * from './token';
+export * from './date';
