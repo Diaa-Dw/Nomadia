e5c7e01fd6dd1b871cd8ce63c92fad1c320b85b6
Diaa-Dw <oava9944@gmail.com>
2025-06-21 21:20:35 +0300

    Remove AdminTablePagination component and its associated types

--- src/containers/AdminTableLayout/components/AdminTablePagination/AdminTablePagination.types.ts
+++
@@ -1,6 +0,0 @@
-export interface AdminTablePaginationProps {
-  page: number;
-  rowsPerPage: number;
-  onPageChange: (page: number) => void;
-  hasNextPage?: boolean;
-}
--- src/containers/AdminTableLayout/components/AdminTablePagination/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as AdminTablePagination } from './AdminTablePagination';
